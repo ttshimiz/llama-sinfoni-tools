99bbafe4b2d28e477bf6743759d1059193af6d80
ttshimiz <tshimizu@astro.umd.edu>
2017-04-13 16:59:41 +0200

    write function to create RGB figures

--- llama_sinfoni_tools.py
+++ llama_sinfoni_tools.py
@@ -1224,3 +1224,68 @@ def calc_pixel_distance(header, center_coord):
     #pa[ind3] = 360.-np.arctan(dx[ind3]/dy[ind3])*180./np.pi
 
     return seps, pa
+
+
+def create_rgb_image(rfile, gfile, bfile, scale=1e21, stretch=12, Q=0.1,
+                     sn_cut=3.0):
+    """
+    Function to create an RGB image from 3 emission line flux maps.
+    Uses astropy.visualization.make_lupton_rgb to generate the RGB values.
+    Uses aplpy to create and show the figure.
+
+    Parameters
+    ----------
+    rfile/gfile/bfile = strings for the FITS files containing the line maps corresponding
+                        to each color
+    scale = float or 3 element array with values to multiply the integrated fluxes by.
+    stretch = float for the linear stretch of the image
+    Q = float for the asinh softening parameter
+    sn_cut = S/N threshold of each line, all pixels with S/N < sn_cut will be set to 0
+
+    For more information on stretch and Q see the documentation for make_lupton_rgb and
+    Lupton (2004).
+
+    Returns
+    -------
+    fig = aplpy.FITSFigure object
+
+    """
+
+    from astropy.visualization import make_lupton_rgb
+
+    hdu_r = fits.open(rfile)
+    hdu_g = fits.open(gfile)
+    hdu_b = fits.open(bfile)
+
+    image_r = hdu_r['int flux'].data
+    image_r_err = hdu_r['int flux error'].data
+
+    image_g = hdu_g['int flux'].data
+    image_g_err = hdu_g['int flux error'].data
+
+    image_b = hdu_b['int flux'].data
+    image_b_err = hdu_b['int flux error'].data
+
+    ind_r = (image_r/image_r_err < sn_cut) | np.isnan(image_r)
+    ind_g = (image_g/image_g_err < sn_cut) | np.isnan(image_g)
+    ind_b = (image_b/image_b_err < sn_cut) | np.isnan(image_b)
+
+    image_r[ind_r] = 0
+    image_g[ind_g] = 0
+    image_b[ind_b] = 0
+
+    if np.isscalar(scale):
+        image_r = image_r*scale
+        image_g = image_g*scale
+        image_b = image_b*scale
+    else:
+        image_r = image_r*scale[0]
+        image_g = image_g*scale[1]
+        image_b = image_b*scale[2]
+
+    rgb = make_lupton_rgb(image_r, image_g, image_b, filename='rgb.png', Q=Q, stretch=stretch)
+
+    fig = aplpy.FITSFigure(hdu_r)
+    fig.show_rgb('rgb.png', interpolation='gaussian', vertical_flip=True)
+
+    return fig
